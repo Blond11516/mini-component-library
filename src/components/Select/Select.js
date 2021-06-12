8019955f12fdbf8b901fbd1b0cb60abf63711ed4
Étienne Lévesque <etienne.levesque16@gmail.com>
2021-06-12 17:06:54 -0400

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,60 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const NativeSelect = styled.select`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  position: absolute;
+`
+
+const PresentationalBit = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 8px;
+  padding: 12px;
+
+  font-size: 1rem;
+
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+
+  &:hover {
+  }
+
+  ${NativeSelect}:focus ~ & {
+    background-color: red;
+  }
+  
+  ${NativeSelect}:hover ~ & {
+    color: ${COLORS.black};
+  }
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`
+
+const ArrowIcon = styled(Icon)`
+  margin-left: 16px;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <ArrowIcon id="chevron-down" size={24} strokeWidth={2} />
+      </PresentationalBit>
+    </Wrapper>
   );
 };
 
