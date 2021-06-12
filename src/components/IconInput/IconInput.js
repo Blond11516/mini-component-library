80b9371ffcf8e16ae0465225932d12df6efaef08
Étienne Lévesque <etienne.levesque16@gmail.com>
2021-06-12 17:55:59 -0400

    IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+
+const STYLES = {
+  small: {
+    height: 18,
+    borderThickness: 1,
+    iconSize: 15,
+    fontSize: 14 / 16
+  },
+  large: {
+    height: 28,
+    borderThickness: 2,
+    iconSize: 24,
+    fontSize: 18 / 16
+  }
+}
+
+const NativeInput = styled.input`
+  padding-left: 28px;
+  height: ${({ size }) => STYLES[size].height}px;
+  width: ${({ width }) => width}px;
+
+  font-size: ${({ size }) => STYLES[size].fontSize}rem;
+  color: ${COLORS.gray700};
+  font-weight: 700;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &::placeholder {
+    font-size: ${({ size }) => STYLES[size].fontSize}rem;
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  border: none;
+  border-bottom: ${({ size }) => STYLES[size].borderThickness}px solid ${COLORS.black};
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+  cursor: text;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+`
 
 const IconInput = ({
   label,
@@ -13,7 +69,17 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const inputRef = useRef(null)
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>
+        {label}
+      </VisuallyHidden>
+      <StyledIcon id={icon} size={STYLES[size].iconSize} onClick={() => inputRef.current.focus()} />
+      <NativeInput type="text" placeholder={placeholder} width={width} size={size} ref={inputRef} />
+    </Wrapper>
+  );
 };
 
 export default IconInput;
